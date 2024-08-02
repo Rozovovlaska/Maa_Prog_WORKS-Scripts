f3bc85707a140e7fef62602c051ad1969f751ffb
Rozovovlaska_Olex1y <106437667+Rozovovlaska@users.noreply.github.com>
2024-08-02 04:19:02 +0300

    Create tstTmain.c

---
+++ tstTmain.c
@@ -0,0 +1,21 @@
+// Online C Compiler - Build, Compile and Run your C programs online in your favorite browser
+
+#include<stdio.h>
+
+int main(){
+    printf("Welcome to Online IDE!! Happy Coding :)")
+    uint8_t vvod;
+    scanf( "&a",vvod);
+    
+    if (vvod==0)
+    printf("Its equal 0000");
+    
+    else{
+    
+    for (int i=0; i>= vvod; i++)
+    printf("%d", i-1);
+    
+}
+    return   0;
+    
+}
