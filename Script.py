2fec04d8acede747a0fac7edba4973df7f7bf861
Rozovovlaska_Olex1y <106437667+Rozovovlaska@users.noreply.github.com>
2024-09-09 04:14:32 +0300

    Add files via upload

--- Script.py
+++ Script.py
@@ -1,5 +1,34 @@
 #!   usr/bin/python3
-a =0
-if a <= 0:
+
+# Scrip to open  URL 
+
+#input
+url=input("Enter the URL:")
+# Input URL link into variablф
+#url_link = input("Enter the URL link: ")
+
+#  is true  lenght
+
+
+if len(url)>0 :
     import webbrowser
-    webbrowser.open  ('https://example.com')
+    #open  URL
+    #webbrowser.open  ('https://example.com') & webbrowser.open ('https://github.com')
+    webbrowser.open (url)
+
+i=8
+def comma_fnc():
+    for i in range(10):
+        print("Its your  link: ",url)
+
+        if len(url) == 0:
+print("Not  Link_!!! ")
+
+
+    else:
+        print("Its  short link!!! ")
+
+
+
+# return ii = comma_fnc()
+
